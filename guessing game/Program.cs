9c023d2cbb2cacccef11bfb365bf97fb822e2d02
Anthony Anderson <antgande@ut.utm.edu>
2020-08-27 17:24:40 -0500

    i hope this works

--- guessing game/Program.cs
+++ guessing game/Program.cs
@@ -7,10 +7,23 @@ namespace guessing_game
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int rnumber = rnd.Next(1, 100);
             Console.WriteLine("GUESSING GAME");
-            Console.WriteLine("I am thinking of a number between 0 and 100");
+            Console.WriteLine("Between what two numbers would you like to pick from?");
+            
+            Console.Write("First number (lowest number): ");
+            string string1 = Console.ReadLine();
+
+            Console.Write("Second number (Highest number): ");
+            string string2 = Console.ReadLine();
+
+            int num1, num2;
+
+            int.TryParse(string1, out num1);
+            int.TryParse(string2, out num2);
+
+            Random rnd = new Random();
+            int rnumber = rnd.Next(num1, num2);
+            Console.WriteLine($"I am thinking of a number between {num1} and {num2}");
             Console.WriteLine("Try your luck and guess my number");
             
             int guess;
