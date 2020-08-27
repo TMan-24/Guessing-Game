0f7657f1c8e8f31c2bb00c36c73edf33ed380359
Anthony Anderson <antgande@ut.utm.edu>
2020-08-27 16:48:44 -0500

    guessing game success

--- guessing game/Program.cs
+++ guessing game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace guessing_game
 {
@@ -6,7 +7,44 @@ namespace guessing_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World and all who inhabit it!");
+            Random rnd = new Random();
+            int rnumber = rnd.Next(1, 100);
+            Console.WriteLine("GUESSING GAME");
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+            Console.WriteLine("Try your luck and guess my number");
+            int guess;
+            int count = 0;
+
+            do
+            {
+                Console.WriteLine();
+                string input = Console.ReadLine();
+                count++;
+               
+               
+                
+                int.TryParse(input, out guess);
+
+                
+                if (guess < rnumber)
+                {
+                    Console.WriteLine($"Your guess of {guess} too low, Try again");
+                    Console.WriteLine();
+
+
+                }
+                else if (guess > rnumber)
+                {
+                    Console.WriteLine($"Your guess of {guess} too high, Try again");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"YOU WON THE NUMBER WAS {guess}");
+                    Console.WriteLine($"It took you {count} tries to guess the correct number");
+                }
+
+            } while (guess != rnumber);
         }
     }
 }
